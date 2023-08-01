f17248d8a081c8f0f960c0de4d2c705321f7f355
Christian De Leon <christiandeleon12693@gmail.com>
2023-07-31 22:09:13 -0400

    Add pipe to bash

--- install.sh
+++ install.sh
@@ -10,6 +10,22 @@ dotfiles_dir=~/dotfiles
 # Location of your backup directory
 backup_dir=~/dotfiles_backup
 
+# Check if the dotfiles repository already exists
+if [ ! -d "${dotfiles_dir}" ]; then
+    # Ask the user for their preference
+    read -p "Would you like to clone the repository using SSH or HTTPS? [SSH/HTTPS]: " method
+
+    # Clone the repository based on their preference
+    if [ "$method" = "SSH" ]; then
+        git clone git@github.com:christian-deleon/dotfiles.git ${dotfiles_dir}
+    elif [ "$method" = "HTTPS" ]; then
+        git clone https://github.com/christian-deleon/dotfiles.git ${dotfiles_dir}
+    else
+        echo "Invalid option. Please run the script again and choose either SSH or HTTPS."
+        exit 1
+    fi
+fi
+
 # Create backup directory if it doesn't exist
 mkdir -p ${backup_dir}
 
