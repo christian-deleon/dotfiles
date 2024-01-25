e2420f5069143217f262eb7a665376f3ccabda39
Christian De Leon <christiandeleon12693@gmail.com>
2024-01-25 15:01:39 -0500

    Install Python and Pip if not avaliable

--- dot.sh
+++ dot.sh
@@ -4,16 +4,38 @@ set -e
 
 ansible_dir=~/dotfiles/ansible
 
-# Check if Ansible is installed
+
+# Check if Python, Pip, and Ansible are installed
 if [[ ! -x "$(command -v ansible)" ]]; then
     echo
-    echo "Installing Ansible..."
+    echo "Ansible is not installed. Installing Ansible..."
+
+    # Check if Python 3 is installed
+    if ! command -v python3 &> /dev/null; then
+        read -p "Python 3 is not installed. Do you want to install Python 3? (y/n): " install_python
+        if [[ "${install_python}" != "y" ]]; then
+            echo "Please install Python 3 and run this script again."
+            exit 1
+        fi
+
+        read -p "Enter the Python minor version (default: 11): " python_minor
+        python_minor=${python_minor:-11}
+
+        # Install Python 3
+        wget -qO - https://gitlab.com/-/snippets/3638671/raw/main/install_python.sh | bash -s -- ${python_minor}
+        do
+    fi
+
+    # Check if pip is installed
     if ! command -v pip &> /dev/null; then
         echo "Pip is not installed. Installing pip..."
         wget -qO - https://bootstrap.pypa.io/get-pip.py | python3        
     fi
+
+    echo "Installing Ansible..."
     python3 -m pip install --user ansible
 fi
+
 
 # Function to update system packages
 update_system() {
@@ -25,6 +47,7 @@ update_system() {
     ansible-playbook -i localhost, ${ansible_dir}/update.yaml
 }
 
+
 # Function to install a tool using Ansible
 install_tool() {
     tool=$1
@@ -35,6 +58,7 @@ install_tool() {
     fi
     ansible-playbook -i localhost, ${ansible_dir}/install-${tool}.yaml
 }
+
 
 # Main logic to handle arguments
 case "$1" in
