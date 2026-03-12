052d4660e14e76af3efff8727ec3a575412efa65
Christian De Leon <christian@deleon.me>
2026-03-11 23:52:09 -0400

    feat(nvim): add <leader>sG to grep hidden files excluding .git

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Grep including hidden files (dotfiles), excluding .git
+vim.keymap.set("n", "<leader>sG", function()
+  Snacks.picker.grep({ args = { "--hidden", "--glob=!.git" } })
+end, { desc = "Grep (hidden files)" })
